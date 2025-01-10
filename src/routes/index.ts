8646f313a9c52b579ff735b488e42e9660b6a3ff
Jeevan Joshi <117514522+JeevanJoshi4434@users.noreply.github.com>
2025-01-10 21:39:24 +0530

    Delete src directory

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router, type Request, type Response } from 'express';
-
-const router = Router();
-
-router.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Welcome to the Index Route!' });
-});
-
-export default router;
--- src/types/mongo.d.ts
+++
@@ -1,4 +0,0 @@
-// for MONGO_URI = mongodb://localhost:27017/farm
-type URI = string;
-
-export { URI };
